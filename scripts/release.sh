d2cac9a6f41052da0ea5258837fd9175cc5ab602
azzamsa <me@azzamsa.com>
2022-08-24 17:19:59 +0700

    build: don't upgrade dependencies without concern

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,9 +14,6 @@ if [ -n "$1" ]; then
     new_version=${1#v} # strip the `v` prefix
     cargo set-version $new_version
 
-    # update Cargo.lock
-    cargo update
-
     # update the changelog
     git-cliff --tag "$1" --sort newest --config configs/cliff.toml > CHANGELOG.md
     # format newly added changelog file
