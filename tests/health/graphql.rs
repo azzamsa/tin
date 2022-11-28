0290a1269d36b6ef045665f6ba6df88300e4a7d9
azzamsa <me@azzamsa.com>
2022-11-28 15:51:23 +0700

    docs: remove outdated comments

--- tests/health/graphql.rs
+++ tests/health/graphql.rs
@@ -12,11 +12,6 @@ pub mod queries {
         pub health: Health,
     }
 
-    // `serde::Deserialize` is needed only to Deserialize
-    // a response from GraphQL. We use the struct here instead
-    // of in `schema.rs` just to avoid duplication.
-    // If `serde::Deserialize` changes cynic behavior, we will
-    // move it to separate struct in `schema.rs`
     #[derive(cynic::QueryFragment, Debug)]
     pub struct Health {
         pub status: String,
