4cf59168a30d5dde582d8a22f09c3c1c70ce4e16
azzamsa <me@azzamsa.com>
2023-09-27 11:57:42 +0700

    refactor: reorder module
    For readability.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 pub mod config;
 pub mod context;
 pub mod db;
-pub mod domain;
-pub mod driver;
 mod error;
 pub mod logger;
 pub mod relay;
 pub mod route;
 pub mod scalar;
 pub mod schema;
+
+pub mod domain;
+pub mod driver;
 
 pub use error::Error;
